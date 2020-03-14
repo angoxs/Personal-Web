58a385ec94376817f39986541abbbbaecb13f517
Angel <angelCL0@outlook.com>
2020-03-14 09:39:53 -0400

    300px

--- src/components/LogosBox.js
+++ src/components/LogosBox.js
@@ -23,6 +23,7 @@ const Logo = styled.div`
   grid-template-columns: repeat(2, auto);
   align-items: center;
   justify-items: center;
+  padding: 10px 10px;
 
   :hover {
     box-shadow: 0 10px 20px rgba(0, 0, 0, 0.15);
@@ -37,7 +38,7 @@ const Logo = styled.div`
   }
 
   @media (max-width: 375px) {
-    width: 280px;
+    width: 300px;
   }
 `
 
